0cf00ad3c8b1f912b3c5504445c16075f8f4a7ba
Natsu <spleonardo7@gmail.com>
2026-06-20 23:28:43 +0200

    Add removeartist command

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from handlers.list_commands import router as list_router
 from handlers.help import router as help_router
 from handlers.artists import router as artists_router
 from handlers.admin import router as admin_router
+from handlers.remove_artist import router as remove_router
 
 from flask import Flask
 import threading
@@ -20,6 +21,7 @@ dp.include_router(list_router)
 dp.include_router(help_router)
 dp.include_router(artists_router)
 dp.include_router(admin_router)
+dp.include_router(remove_router)
 
 app = Flask(__name__)
 
