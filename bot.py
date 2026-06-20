c2add7aec8af803b2cbea9de660d7ea8beda7e22
Natsu <spleonardo7@gmail.com>
2026-06-20 23:07:46 +0200

    Add Flask keepalive server

--- bot.py
+++ bot.py
@@ -7,7 +7,9 @@ from handlers.list_commands import router as list_router
 from handlers.help import router as help_router
 from handlers.artists import router as artists_router
 from handlers.admin import router as admin_router
-from handlers.remove_artist import router as remove_router
+
+from flask import Flask
+import threading
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
@@ -18,7 +20,27 @@ dp.include_router(list_router)
 dp.include_router(help_router)
 dp.include_router(artists_router)
 dp.include_router(admin_router)
-dp.include_router(remove_router)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Vinyl Alert Bot online!"
+
+
+def run_web():
+    app.run(
+        host="0.0.0.0",
+        port=10000
+    )
+
 
 if __name__ == "__main__":
+
+    threading.Thread(
+        target=run_web,
+        daemon=True
+    ).start()
+
     dp.run_polling(bot)
